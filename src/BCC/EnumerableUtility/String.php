94448d02bc32be3c577ef0921fecb6acd33c87d5
Michel Salib <michelsalib@hotmail.com>
2012-10-03 08:32:42 -0700

    Made string class more compatible

--- src/BCC/EnumerableUtility/String.php
+++ src/BCC/EnumerableUtility/String.php
@@ -86,22 +86,22 @@ class String implements IEnumerable
     public function endsWith($value, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            $pos = \strripos($this->string, $value);
+            $pos = \strripos($this->string, (string)$value);
         }
         else {
-            $pos = \strrpos($this->string, $value);
+            $pos = \strrpos($this->string, (string)$value);
         }
 
-        return $pos === \strlen($this->string) - \strlen($value);
+        return $pos === \strlen($this->string) - \strlen((string)$value);
     }
 
     public function startsWith($value, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            $pos = \stripos($this->string, $value);
+            $pos = \stripos($this->string, (string)$value);
         }
         else {
-            $pos = \strpos($this->string, $value);
+            $pos = \strpos($this->string, (string)$value);
         }
 
         return $pos === 0;
@@ -110,10 +110,10 @@ class String implements IEnumerable
     public function equals($value, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            $cmp = \strcasecmp($this->string, $value);
+            $cmp = \strcasecmp($this->string, (string)$value);
         }
         else {
-            $cmp = \strcmp($this->string, $value);
+            $cmp = \strcmp($this->string, (string)$value);
         }
 
         return $cmp === 0;
@@ -123,16 +123,16 @@ class String implements IEnumerable
     {
         $args = \array_slice(\func_get_args(), 1);
 
-        return new String(\vsprintf($string, $args));
+        return new String(\vsprintf((string)$string, $args));
     }
 
     public function indexOf($value, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            $pos = \stripos($this->string, $value);
+            $pos = \stripos($this->string, (string)$value);
         }
         else {
-            $pos = \strpos($this->string, $value);
+            $pos = \strpos($this->string, (string)$value);
         }
 
         return $pos !== false ? $pos : -1;
@@ -148,26 +148,26 @@ class String implements IEnumerable
 
     public static function isNullOrEmpty($string)
     {
-        return \strlen($string) === 0;
+        return \strlen((string)$string) === 0;
     }
 
     public static function isNullOrWhiteSpace($string)
     {
-        return \strlen(\trim($string)) === 0;
+        return \strlen(\trim((string)$string)) === 0;
     }
 
     public static function concatenate($separator, array $strings)
     {
-        return new String(\implode($separator, $strings));
+        return new String(\implode((string)$separator, $strings));
     }
 
     public function lastIndexOf($value, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            $pos = \strripos($this->string, $value);
+            $pos = \strripos($this->string, (string)$value);
         }
         else {
-            $pos = \strrpos($this->string, $value);
+            $pos = \strrpos($this->string, (string)$value);
         }
 
         return $pos !== false ? $pos : -1;
@@ -199,25 +199,25 @@ class String implements IEnumerable
     public function replace($old, $new, $ignoreCase = false)
     {
         if ($ignoreCase) {
-            return new String(\str_ireplace($old, $new, $this->string));
+            return new String(\str_ireplace((string)$old, (string)$new, $this->string));
         }
         else {
-            return new String(\str_replace($old, $new, $this->string));
+            return new String(\str_replace((string)$old, (string)$new, $this->string));
         }
     }
 
     public function split($separator)
     {
-        return \explode($separator, $this->string);
+        return \explode((string)$separator, $this->string);
     }
 
     public function subString($startIndex, $length = null)
     {
         if ($length === null) {
-            return new String(\substr($this->string, $startIndex));
+            return new String(\substr($this->string, (string)$startIndex));
         }
 
-        return new String(\substr($this->string, $startIndex, $length));
+        return new String(\substr($this->string, (string)$startIndex, $length));
     }
 
     public function toLower()
