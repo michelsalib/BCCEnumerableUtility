59a272800e2ddce07453071b2571d78901700f55
Michel Salib <michelsalib@hotmail.com>
2013-09-29 23:00:29 +0200

    Adding test on expression resolver

---
+++ src/BCC/EnumerableUtility/Tests/Resolver/ExpressionResolverTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BCC\EnumerableUtilityUtility\Tests\Resolver;
+
+use BCC\EnumerableUtility\Resolver\ExpressionResolver;
+
+class ExpressionResolverTest extends \PHPUnit_Framework_TestCase
+{
+    public function testResolvePath()
+    {
+        // ARRANGE
+        $resolver = new ExpressionResolver();
+
+        // ACT
+        $result = $resolver->resolve(['i' => 'i["city"]']);
+
+        // ASSERT
+        $this->assertSame('Paris', $result(['city' => 'Paris']));
+    }
+
+    public function testResolveSquare()
+    {
+        // ARRANGE
+        $resolver = new ExpressionResolver();
+
+        // ACT
+        $result = $resolver->resolve(['i' => 'i * i']);
+
+        // ASSERT
+        $this->assertSame(4, $result(2));
+    }
+
+    public function testResolveWithVariables()
+    {
+        // ARRANGE
+        $resolver = new ExpressionResolver();
+
+        // ACT
+        $result = $resolver->resolve([
+            'i' => 'i * m',
+            'm' => 2,
+        ]);
+
+        // ASSERT
+        $this->assertSame(4, $result(2));
+    }
+}
