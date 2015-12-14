7525c36693dc8d96638ebeb6c0c28eee9c92f620
Jorge García <jorge.garcia@fon.com>
2015-12-14 13:01:13 +0100

    Added String class due to compatibility

---
+++ src/String.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BCC\EnumerableUtility;
+
+/**
+ * Class String
+ *
+ * @package BCC\EnumerableUtility
+ *
+ * @deprecated
+ */
+class String extends Stringer
+{
+    public function __construct($string = null)
+    {
+        @trigger_error(
+            'Class BCC\EnumerableUtility\String is deprecated. Please use BCC\EnumerableUtility\Stringer instead.',
+            E_USER_DEPRECATED
+        );
+        parent::__construct($string);
+    }
+
+    public static function create($string = null)
+    {
+        @trigger_error(
+            'Class BCC\EnumerableUtility\String is deprecated. Please use BCC\EnumerableUtility\Stringer instead.',
+            E_USER_DEPRECATED
+        );
+
+        return parent::create($string);
+    }
+}
